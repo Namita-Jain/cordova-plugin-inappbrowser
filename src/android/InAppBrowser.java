d44d1281f462eae6cb0e04f67cd60000392fd17a
Namita Jain <namita.jain@joshtechnologygroup.com>
2021-02-02 14:57:45 +0530

    Fix

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -86,6 +86,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.StringTokenizer;
+import src/android/InAppBrowserViewUtil.kt;
 
 @SuppressLint("SetJavaScriptEnabled")
 public class InAppBrowser extends CordovaPlugin {
